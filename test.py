0b7820852544c4c30820c6b62d25a0543aa3f441
Matheus Guimarães <100164786+garotodeti@users.noreply.github.com>
2024-02-09 17:40:27 -0300

    Inserção dos arquivos

---
+++ test.py
@@ -0,0 +1,73 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'Datezinho?'
+    page.window_width = 600
+    page.window_height = 600
+    page.window_resizable = False
+    page.padding = 100
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.theme_mode = 'light'
+    page.bgcolor = '#B3CFFF'
+    page.update()
+
+    text = ft.Text(
+            "Clique no ♥ para uma surpresa!",
+            size=20,
+            color=ft.colors.BLACK,
+            weight=ft.FontWeight.BOLD,
+            font_family="Montserrat"
+        )    
+    envolpe_img = ft.Image(
+                    src=f"https://raw.githubusercontent.com/liviavianna/pedido_date_py/main/envelope.png",
+                    width=200,
+                    height=200
+    )
+    def close_dlg(e):
+        dlg_yes.open = False
+        page.update()
+
+    def close_page(e):
+        page.dialog = dlg_yes
+        dlg_yes.open = True
+        page.update()
+    dlg_yes = ft.AlertDialog(
+        title=ft.Text("escreva algo ♥"),
+        content=ft.Text("Escreva algo"),
+        actions=[
+            ft.TextButton("X", on_click=close_dlg),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        on_dismiss=lambda e: print("Modal dialog dismissed!"),
+    )
+
+    def open_dlg_modal(e):
+        page.dialog = dlg_modal
+        dlg_modal.open = True
+        page.update()
+    dlg = ft.AlertDialog(
+        content=ft.Text("Diga sim por favor vai skaksk")
+    )
+    
+    def open_dlg(e):
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+    dlg_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Diga sim"),
+        content=ft.Text("Aceitas ir em um date comigo? 🌹"),
+        actions=[
+            ft.TextButton("Sim", on_click=close_page),
+            ft.TextButton("Não", on_click=open_dlg),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        on_dismiss=lambda e: print("Modal dialog dismissed!"),
+    )
+    
+    b = ft.IconButton(
+        icon=ft.icons.FAVORITE, icon_color="red", on_click=open_dlg_modal, data=0, icon_size=40
+    )   
+    page.add(text, envolpe_img, b)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
